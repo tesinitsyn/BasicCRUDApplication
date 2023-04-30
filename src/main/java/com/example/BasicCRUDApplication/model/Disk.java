b5d90fb27c91a16c45b1f2892f51bb210e40f7fd
tesinitsyn <sitiev174@gmail.com>
2023-05-01 00:26:02 +0300

    Added variables, getter, setters in Disk

--- src/main/java/com/example/BasicCRUDApplication/model/Disk.java
+++ src/main/java/com/example/BasicCRUDApplication/model/Disk.java
@@ -1,4 +1,75 @@
 package com.example.BasicCRUDApplication.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.Table;
+
+@Entity
+@Table(name = "disks")
 public class Disk {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String genre;
+
+    @Column
+    private String rating;
+
+    @Column
+    private String directors;
+
+    @Column
+    private String cover;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
 }
